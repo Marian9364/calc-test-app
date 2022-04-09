5214a9ceb128dea50b388688dc53eb0c3f052708
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-04-09 15:54:01 +0200

    history function
    history function - collecting up to 5 results

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const historyUL = document.getElementById("history");
+const historyTitle = document.getElementById("history-title");
 const fieldDiv = document.getElementById("root");
 const resultField = document.getElementById("result");
 
@@ -5,10 +7,14 @@ let firstField = document.getElementById("first-number");
 let secondField = document.getElementById("second-number");
 let operator = document.getElementById("operator");
 let calcBtn = document.getElementById("calc-btn");
+let numberOfLength = 0;
 
+historyTitle.style.display = "none";
 calcBtn.addEventListener("click", onClick);
 
-function onClick() {
+function onClick(e) {
+  e.preventDefault();
+
   let firstFieldValue = Number(firstField.value);
   let secondFieldValue = Number(secondField.value);
   let operatorValue = operator.value;
@@ -36,4 +42,27 @@ function onClick() {
   firstField.value = "";
   secondField.value = "";
   operator.value = "";
+
+  historyTitle.style.display = "inline-block";
+
+  if (resultField.innerHTML != "") {
+    if (numberOfLength < 5) {
+      let li = document.createElement("li");
+      let lastNumber = resultField.innerText;
+
+      li.innerText = lastNumber;
+      historyUL.appendChild(li);
+
+      numberOfLength = historyUL.children.length;
+      console.log(numberOfLength);
+    } else {
+      historyUL.replaceChildren();
+
+      let li = document.createElement("li");
+      let lastNumber = resultField.innerText;
+      li.innerText = lastNumber;
+      historyUL.appendChild(li);
+      numberOfLength = historyUL.children.length;
+    }
+  }
 }
